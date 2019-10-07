3083c88c1f9d80994ba87cd3a3b8f8ac9a9e1b8b
Julia Signell <jsignell@gmail.com>
2019-10-07 09:56:24 -0400

    Renaming app.ipynb (#2)

--- panelserverextension.py
+++ panelserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-    """serve the app.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "app.ipynb", "--allow-websocket-origin=*"])
+    """serve the nyc_taxi.ipynb directory with bokeh server"""
+    Popen(["panel", "serve", "nyc_taxi.ipynb", "--allow-websocket-origin=*"])
